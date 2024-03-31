dfcc10b8d286181ad65c97a12efad7d5e6997aac
AllisonFS <franciscoallison007@gmail.com>
2024-03-30 22:04:13 -0300

    Player de música!
    Neste projeto, empenhando uma longa pesquisa, fiz um player de musica simples. Nele aprendi conceitos importantes de Javascrip e Css.

---
+++ MusicPlayer/data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'Mete um block nele',
+        path: 'musics/song2.mp3',
+        artist: 'João gomes',
+        cover: 'images/Mete um block nele-image.jpg'
+    },
+    {
+        name: 'X1',
+        path: 'musics/song1.mp3',
+        artist: 'Mc Cabelinho',
+        cover: 'images/x1-image.jpg'
+    },
+    {
+        name: 'Amor de fim de noite',
+        path: 'musics/song3.mp3',
+        artist: 'Orochi',
+        cover: 'images/music3.jpg'
+    },
+    {
+        name: 'Deixa eu te chamar de meu amor',
+        path: 'musics/song4.mp3',
+        artist: 'Aldair Playboy',
+        cover: 'images/music4.jpg'
+    },
+    {
+        name: 'Animals',
+        path: 'musics/song5.mp3',
+        artist: 'Marron 5',
+        cover: 'images/music5.jpg'
+    },
+]
